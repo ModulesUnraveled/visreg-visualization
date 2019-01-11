043972e69f6547f5fb8fde39240e074eba461f65
Brian Lewis <brian@modulesunraveled.com>
2019-01-11 06:00:42 -0600

    Initial commit.

---
+++ visreg.js
@@ -0,0 +1,9 @@
+function imageComparator() {
+  sizeDiv = $('#slider-ic').val();
+  $('.image-original').css('width',sizeDiv + '%');
+  $('#cursor-ic').css('left',sizeDiv + '%');
+}
+
+$('#slider-ic').on('mousemove touchmove click', function(){
+  imageComparator();
+});
